b89acdaa50e56f82c97b904637e64baf0707344a
Ian Hu <ianhu952@hotmail.com>
2017-03-01 13:18:56 -0800

    fix

--- src/cli.js
+++ src/cli.js
@@ -28,9 +28,7 @@ const argPaths = argv._;
 const dirPaths = p => glob.sync('/**/*.js?(x)', { root: p })
   .filter(src => src.indexOf('__tests__') === -1);
 
-const unitPaths = _.flatten(
-  argPaths.map(p => (fs.lstatSync(p).isDirectory() ? dirPaths(p) : p)),
-);
+const unitPaths = _.flatten(argPaths.map(p => (fs.lstatSync(p).isDirectory() ? dirPaths(p) : p)));
 
 const lazyManaged = (specPath) => {
   const specSrc = fs.readFileSync(specPath, 'utf8');
